6f8fc6562a3b5c15ab827155a47cc07fbe280936
claudiasrn <perezserranoclaudia@gmail.com>
2026-09-02 20:18:02 +0200

    feat: mount index router in app

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const path = require("node:path");
 const express = require("express");
+const indexRouter = require("./routes/indexRouter");
 
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use("/", indexRouter);
+
 app.listen(8080, () => {
 	console.log("Server running on port 8080");
-})
+});
