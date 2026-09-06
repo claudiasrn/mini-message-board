e8ea27bf4a3024a7e85401ca4cb32dc19b462b15
claudiasrn <perezserranoclaudia@gmail.com>
2026-09-06 16:00:25 +0200

    refactor: wire router to use messages controller

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,30 +1,10 @@
-const express = require("express");
-const router = express.Router();
+const { Router } = require("express");
+const messagesController = require("../controllers/messagesController")
+const router = Router();
 
-const messages = [
-	{ text: "Hi there!", user: "Amando", added: new Date() },
-	{ text: "Hello World!", user: "Charles", added: new Date() },
-];
-
-router.get("/", (req, res) => {
-	res.render("index", { messages: messages });
-});
-
-router.get("/new", (req, res) => {
-	res.render("form");
-});
-
-router.post("/new", (req, res) => {
-	const userName = req.body.name;
-	const messageText = req.body.messageText;
-	messages.push({ text: messageText, user: userName, added: new Date() });
-
-	res.redirect("/");
-});
-
-router.get("/message/:id", (req, res) => {
-	const index = Number(req.params.id);
-	res.render("message", { message: messages[index], id: index });
-});
+router.get("/", messagesController.messagesGet);
+router.get("/new", messagesController.formGet);
+router.post("/new", messagesController.messagePost);
+router.get("/message/:id", messagesController.messageGet);
 
 module.exports = router;
