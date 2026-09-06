62a5d6b931b7a95e6bfd3ca3849e42e3f4b24a48
claudiasrn <perezserranoclaudia@gmail.com>
2026-09-06 16:29:56 +0200

    feat: add server-side validation for message form

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,4 +1,5 @@
 const db = require("../db/queries");
+const { body, validationResult, matchedData } = require("express-validator");
 
 async function messagesGet(req, res) {
 	const messages = await db.getAllMessages();
@@ -6,26 +7,50 @@ async function messagesGet(req, res) {
 }
 
 function formGet(req, res) {
-    res.render("form");
-}
-
-async function messagePost(req, res) {
-    const userName = req.body.name;
-    const messageText = req.body.messageText;
-    await db.addMessage(userName, messageText);
-
-    res.redirect("/");
+	res.render("form");
 }
 
 async function messageGet(req, res) {
-    const id = Number(req.params.id);
-    const message = await db.getMessageById(id);
-    res.render("message", {message: message});
+	const id = Number(req.params.id);
+	const message = await db.getMessageById(id);
+	res.render("message", { message: message });
 }
 
+const validateMessage = [
+	body("name")
+		.trim()
+		.isAlpha()
+		.withMessage("User name must only contain letters")
+		.isLength({ min: 1, max: 200 })
+		.withMessage("User name must be minimum 1 and maximum 200 characters"),
+	body("messageText")
+		.trim()
+		.isLength({ min: 1, max: 200 })
+		.withMessage("Message must be minimum 1 and maximum 200 characters"),
+];
+
+const messagePost = [
+	validateMessage,
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).render("form", {
+				errors: errors.array(),
+				name: req.body.name,
+				messageText: req.body.messageText,
+			});
+		}
+
+		const { name, messageText } = matchedData(req);
+		await db.addMessage(name, messageText);
+
+		res.redirect("/");
+	},
+];
+
 module.exports = {
-    messageGet,
-    formGet,
-    messagePost,
-    messagesGet
-}
+	messageGet,
+	formGet,
+	messagePost,
+	messagesGet,
+};
